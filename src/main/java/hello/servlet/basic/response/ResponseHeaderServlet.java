a6a3dee1a0e2bfe571a2ca8c087104a82fac6703
core <jinjs6@gmail.com>
2023-01-25 19:00:58 +0900

    feat: response 헤더 편의 메서드 content 작성

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -22,7 +22,19 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setHeader("Pragma", "no-cache");
         response.setHeader("my-header", "hello"); // 내가 만든 헤더
 
+        //[Header 편의 메서드]
+        content(response);
+
         PrintWriter writer = response.getWriter();
         writer.println("안녕하세요");
+    }
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
     }
 }

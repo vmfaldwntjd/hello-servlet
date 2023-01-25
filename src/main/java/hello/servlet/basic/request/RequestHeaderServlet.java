41f879132700562ddc913616f7ef0bb24850c1df
core <jinjs6@gmail.com>
2023-01-25 16:59:56 +0900

    refactor: http 헤더 편리한 조회 부분 코드 위치 수정

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -45,29 +45,27 @@ public class RequestHeaderServlet extends HttpServlet {
     //Header 편리한 조회
     private void printHeaderUtils(HttpServletRequest request) { System.out.println("--- Header 편의 조회 start ---");
         System.out.println("[Host 편의 조회]");
-        System.out.println("request.getServerName() = " +
-            request.getServerName()); //Host 헤더
-        System.out.println("request.getServerPort() = " +
-        request.getServerPort()); //Host 헤더
+        System.out.println("request.getServerName() = " + request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " + request.getServerPort()); //Host 헤더
         System.out.println();
+
         System.out.println("[Accept-Language 편의 조회]");
         request.getLocales().asIterator()
                 .forEachRemaining(locale -> System.out.println("locale = " + locale));
         System.out.println("request.getLocale() = " + request.getLocale());
         System.out.println();
+
         System.out.println("[cookie 편의 조회]");
         if (request.getCookies() != null) {
             for (Cookie cookie : request.getCookies()) {
                 System.out.println(cookie.getName() + ": " + cookie.getValue());
             } }
         System.out.println();
+
         System.out.println("[Content 편의 조회]");
-        System.out.println("request.getContentType() = " +
-                request.getContentType());
-        System.out.println("request.getContentLength() = " +
-                request.getContentLength());
-        System.out.println("request.getCharacterEncoding() = " +
-                request.getCharacterEncoding());
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
         System.out.println("--- Header 편의 조회 end ---");
         System.out.println();
     }

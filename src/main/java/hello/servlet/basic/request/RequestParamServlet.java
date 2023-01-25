bae27186673121d5e6e610d6cdf21365e404a033
core <jinjs6@gmail.com>
2023-01-25 17:16:26 +0900

    feat: 단일 파라미터 조회 기능 작성

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -25,5 +25,13 @@ public class RequestParamServlet extends HttpServlet {
 
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
+
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        String age = request.getParameter("age");
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] - end");
+        System.out.println();
     }
 }

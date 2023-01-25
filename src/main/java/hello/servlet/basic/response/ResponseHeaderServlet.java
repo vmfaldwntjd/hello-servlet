94b18348204ad2d81dccb89c51a1ead633356861
core <jinjs6@gmail.com>
2023-01-25 18:49:48 +0900

    feat: ResponseHeaderServlet.java에 status-line 및 response-headers 내용 작성

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -1,15 +1,28 @@
 package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK); // 전송 상태 확인
 
+        //[response-headers]
+        response.setHeader("Content-Type", "text/plain"); // 텍스트 타입
+        response.setHeader("Cache-control", "no-cache, no-store, must-revalidate"); // 캐시 무효화
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello"); // 내가 만든 헤더
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
     }
 }

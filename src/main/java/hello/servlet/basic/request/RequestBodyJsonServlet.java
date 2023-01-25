11bbf3df6e6c8e28be30c7ea4ad6d1afe53c5a3e
core <jinjs6@gmail.com>
2023-01-25 18:33:37 +0900

    feat: json 형태의 메시지 바디를 객체로 추출하는 기능 작성

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.ServletException;
@@ -14,11 +16,19 @@ import java.nio.charset.StandardCharsets;
 @WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         System.out.println("messageBody = " + messageBody);
+        System.out.println();
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.getUserName() = " + helloData.getUserName());
+        System.out.println("helloData.getAge() = " + helloData.getAge());
 
         response.getWriter().write("ok");
     }

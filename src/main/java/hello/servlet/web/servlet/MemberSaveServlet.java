cc9f27f8b9b099eb4ae79c69b67d7da0bbca046f
core <jinjs6@gmail.com>
2023-01-26 16:14:00 +0900

    feat: 등록한 멤버를 저장하는 기능 작성

--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -9,8 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "memberSaveServlet", urlPatterns = "servlet/members/save")
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
 public class MemberSaveServlet extends HttpServlet {
 
     private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
@@ -23,5 +24,21 @@ public class MemberSaveServlet extends HttpServlet {
 
         Member member = new Member(userName, age);
         memberRepository.save(member);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter writer = response.getWriter();
+        writer.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" + "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                "    <li>id=" + member.getId() + "</li>\n" +
+                "    <li>username=" + member.getUserName() + "</li>\n" +
+                " <li>age=" + member.getAge() + "</li>\n" + "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" + "</body>\n" +
+                "</html>");
+
     }
 }

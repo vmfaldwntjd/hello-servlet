41a7f16f358c44b2c24fe681712590ffa18283eb
core <jinjs6@gmail.com>
2023-01-26 16:00:56 +0900

    feat: 회원 정보 입력 저장 기능 작성

--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.servlet;
 
+import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 
 import javax.servlet.ServletException;
@@ -16,6 +17,11 @@ public class MemberSaveServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String userName = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
 
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
     }
 }

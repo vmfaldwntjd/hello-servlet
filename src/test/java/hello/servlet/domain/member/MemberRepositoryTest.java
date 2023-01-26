fe55b03223b067c0a6dbc411b98a83701d7e60d9
core <jinjs6@gmail.com>
2023-01-26 15:31:10 +0900

    test: MemberRepository 파일의 save기능 테스트

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,4 +1,28 @@
 package hello.servlet.domain.member;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
 public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore(); //테스트 끝나면 깔끔하게 초기화 하기
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        Assertions.assertThat(findMember).isEqualTo(savedMember);
+    }
 }

d7b572727f692b3fab0be952c241310d4e17d8ac
core <jinjs6@gmail.com>
2023-01-29 16:49:38 +0900

    feat: 컨트롤러에서 뷰로 데이터를 넘겨줄 로직을 작성

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.servletmvc;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,9 @@ public class MvcMemberFormServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
     }
 }
+ㅌ

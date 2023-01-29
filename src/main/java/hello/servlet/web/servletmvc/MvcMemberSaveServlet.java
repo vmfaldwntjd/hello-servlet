5946f0b896e834c93580fb54731eb12a86d0cade
core <jinjs6@gmail.com>
2023-01-29 17:14:57 +0900

    feat: 멤버 정보 저장 비즈니스 로직 작성

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -1,5 +1,8 @@
 package hello.servlet.web.servletmvc;
 
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,8 +13,16 @@ import java.io.IOException;
 @WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
 public class MvcMemberSaveServlet extends HttpServlet {
 
+    private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String userName = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+        
     }
 }

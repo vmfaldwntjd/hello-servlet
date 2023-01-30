97c727d4e3f9a861f3a3852e44612b88b145edb0
core <jinjs6@gmail.com>
2023-01-30 12:26:08 +0900

    feat: MemberSaveController생성 및 저장 로직 작성

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveController.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveController implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관하기
+        request.setAttribute("member", member);
+
+        //데이터 뷰로 넘겨주기
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}

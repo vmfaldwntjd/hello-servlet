5b4a5a0517da6a390e8bf6da9792f0be379a3edb
core <jinjs6@gmail.com>
2023-01-30 12:26:46 +0900

    refactor: MemberSaveController 파일 이름을 MemberSaveControllerV1로 변경

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MemberSaveController implements ControllerV1 {
+public class MemberSaveControllerV1 implements ControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
 

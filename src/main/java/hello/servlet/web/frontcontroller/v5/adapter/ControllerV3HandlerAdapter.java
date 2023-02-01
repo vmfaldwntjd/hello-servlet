0c655ee57b6bce56e114197bcb1e3e3c959f92cb
core <jinjs6@gmail.com>
2023-02-01 18:36:27 +0900

    feat: 컨트롤러 객체 여부 확인 로직 작성

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -1,2 +1,23 @@
-package hello.servlet.web.frontcontroller.v5.adapter;public class ControllerV3HandlerAdapter {
+package hello.servlet.web.frontcontroller.v5.adapter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3); // 원하는 컨트롤러 객체인지 확인
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return null;
+    }
 }

316a240d9b5e1b052812c7a0e47228c646ec8292
core <jinjs6@gmail.com>
2023-02-02 15:07:14 +0900

    refactor: RequestMapping 애노테이션을 통해서 url 중복 부분 제거

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -11,17 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
+@RequestMapping("/springmvc/v2/members")
 public class SpringMemberControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
 
-    @RequestMapping("/springmvc/v2/members/new-form")
+    @RequestMapping("/new-form")
     public ModelAndView newForm() {
         return new ModelAndView("new-form");
     }
 
 
-    @RequestMapping("/springmvc/v2/members/save")
+    @RequestMapping("/save")
     public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String userName = request.getParameter("userName");
         int age = Integer.parseInt(request.getParameter("age"));
@@ -35,7 +36,7 @@ public class SpringMemberControllerV2 {
         return modelAndView;
     }
 
-    @RequestMapping("/springmvc/v2/members")
+    @RequestMapping("")
     public ModelAndView members() {
         List<Member> members = memberRepository.findAll();
 

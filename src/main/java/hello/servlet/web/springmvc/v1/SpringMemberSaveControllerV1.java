121650b53503db4e7fce9de456e7eff16644a689
core <jinjs6@gmail.com>
2023-02-02 14:50:20 +0900

    feat: 스프링 mvc를 적용한 회원 정보 저장 로직 작성

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -1,2 +1,30 @@
-package hello.servlet.web.springmvc.v1;public class SpringMemberSaveControllerV1 {
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(Map<String, String> paramMap) {
+        String userName = paramMap.get("userName");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(userName, age);
+
+        memberRepository.save(member);
+
+        ModelAndView modelAndView = new ModelAndView("save-result");
+        modelAndView.addObject("member", member);
+        return modelAndView;
+    }
 }

cf33b6078b1fae32af403422995e10a27edf95d5
core <jinjs6@gmail.com>
2023-02-02 14:56:41 +0900

    fix: SpringMemberSaveControllerV1에서 process의 파라미터를 변경 및 적용

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -2,12 +2,12 @@ package hello.servlet.web.springmvc.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SpringMemberSaveControllerV1 {
@@ -15,9 +15,9 @@ public class SpringMemberSaveControllerV1 {
     private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
 
     @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(Map<String, String> paramMap) {
-        String userName = paramMap.get("userName");
-        int age = Integer.parseInt(paramMap.get("age"));
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String userName = request.getParameter("userName");
+        int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(userName, age);
 

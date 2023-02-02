845c399d028c3905d30cb3985fb07e4a93bdf756
core <jinjs6@gmail.com>
2023-02-02 14:54:27 +0900

    feat: 스프링 mvc를 적용한 회원 목록 조회 로직 작성

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberLIstControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberLIstControllerV1.java
@@ -1,2 +1,25 @@
-package hello.servlet.web.springmvc.v1;public class SpringMemberLIstControllerV1 {
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberLIstControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이기 때문에 getInstance로 받아오기
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("members");
+        modelAndView.addObject("members", members);
+
+        return modelAndView;
+    }
 }
